43905aa4752ef4635818d36e6d4143d33ca4ada2
Evalyn Berleant <eberleant@gmail.com>
2020-02-14 13:38:17 -0500

    Change controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#home'
-  get '/dashboard', to: 'artist#dashboard'
-  get '/storefront', to: 'artist#storefront'
+  get '/dashboard', to: 'artists#dashboard'
+  get '/storefront', to: 'artists#storefront'
 end
