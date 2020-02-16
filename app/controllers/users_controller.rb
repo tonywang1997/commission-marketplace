5ed2082f86cb2b93ad704ae0d646bddd286336b4
Evalyn Berleant <eberleant@gmail.com>
2020-02-16 01:37:52 -0500

    Create basic login, logout, and authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def storefront
+    @user = User.find_by(user_name: params[:user_name])
   end
 
   def edit
@@ -26,5 +33,17 @@ class UsersController < ApplicationController
       # raise an error if :user is not an attribute of params hash
       # return hash with attributes :user_name, :email_address, :password, :password_confirmation
       params.require(:user).permit(:user_name, :email_address, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(edit_user_url(current_user)) unless @user == current_user
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#home'
-  get '/register',    to: 'users#new'
-  # get '/dashboard',   to: 'users#edit'
-  # get '/storefront',  to: 'users#show'
-  resources :users
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/storefront/:user_name', to: 'users#storefront'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
+  resources :users
 end
 
 
