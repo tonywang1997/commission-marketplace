1f8e84c0cafc03b08499dcc3ab3938795efd9654
Evalyn Berleant <eberleant@gmail.com>
2020-02-23 23:16:22 -0500

    Refresh modal contents each time

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -2,6 +2,6 @@ document.addEventListener("turbolinks:load", function () {
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-toggle="popover"]').popover();
     $('.btn-group .btn.disabled').click(function(e) {
-         return false;
+      return false;
     });
 });
