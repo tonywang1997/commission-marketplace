e53068340c8b87864b93ba5ec6627aaf90a4f059
Evalyn Berleant <eberleant@gmail.com>
2020-03-09 20:07:00 -0400

    Remove static assets line from development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.serve_static_assets = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
