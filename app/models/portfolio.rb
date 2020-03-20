a1ec8c89254f5c6f8959d6df496f1897916c06d3
Evalyn Berleant <eberleant@gmail.com>
2020-03-20 06:05:08 -0400

    Bring back AWS/image upload changes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,9 @@
 class Portfolio < ApplicationRecord
   belongs_to :user
-  has_many :has_images
   has_many :has_tags
   has_many :images, through: :has_images
   has_many :tags, through: :has_tags
 
   scope :tagged, -> (tags) { joins(:tags).where("tags.tag_name IN (?)", tags).group("portfolios.id").having('count(*) = ?', tags.count) }
+  has_many_attached :files
 end
