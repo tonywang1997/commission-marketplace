0661023dc56db354646efec2395f631821420be4
YifanLeng <yifanleng@brandeis.edu>
2020-04-13 01:52:28 -0400

    finished storefront and portfolio show page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,12 +1,14 @@
 class PortfoliosController < ApplicationController
   include ApplicationHelper
+  include SessionsHelper
   before_action :logged_in_user
   before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
 
-  # GET /portfolios
-  # GET /portfolios.json
+  # /storefront/:user_id
   def index
-    @portfolios = Portfolio.all
+    @user ||= User.find_by(id: session[:user_id])
+    @user_portfolios = @user.portfolios
+    
   end
 
   # GET /portfolios/1
@@ -14,7 +16,7 @@ class PortfoliosController < ApplicationController
   def show
   end
 
-  # GET /portfolios/new
+  # GET /submit
   def new
     @portfolio = Portfolio.new
     # create an empty tag child shown to the user
