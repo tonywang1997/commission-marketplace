472b2a6d0f08e67fe7ed6992ea0d624774ab4760
Evalyn Berleant <eberleant@gmail.com>
2020-04-22 06:20:14 -0400

    Fixed bugs in outdated fixtures/views

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -17,7 +17,7 @@ class TagsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create tag" do
     assert_difference('Tag.count') do
-      post tags_url, params: { tag: { tag_name: @tag.tag_name, tag_id: @tag.tag_id } }
+      post tags_url, params: { tag: { tag_name: @tag.tag_name } }
     end
 
     assert_redirected_to tag_url(Tag.last)
@@ -34,7 +34,7 @@ class TagsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update tag" do
-    patch tag_url(@tag), params: { tag: { tag_name: @tag.tag_name, tag_id: @tag.tag_id } }
+    patch tag_url(@tag), params: { tag: { tag_name: @tag.tag_name } }
     assert_redirected_to tag_url(@tag)
   end
 
