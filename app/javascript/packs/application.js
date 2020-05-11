f22d29e0d9d27c8b52db3f6692a2b084c9b5ff5e
Thomas Pei <ttzpei@gmail.com>
2020-05-11 12:20:21 -0400

    removed jquery_ujs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 require("jquery");
-require("jquery_ujs");
 import $ from 'jquery';
 global.$ = jQuery;
 
