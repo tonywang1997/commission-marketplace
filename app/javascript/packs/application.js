84302a9b4190aa1bd381e0e4c331fae609aac1a5
Thomas Pei <ttzpei@gmail.com>
2020-05-11 12:13:41 -0400

    Fixed a typo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 require("jquery");
-require("jquery_ujs")
+require("jquery_ujs");
 import $ from 'jquery';
 global.$ = jQuery;
 
